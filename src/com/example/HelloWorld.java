992ec3169ebf32d312606dd7d40200b6f1504d06
[yousincha] <[yousincha@naver.com]>
2024-04-24 09:48:34 +0900

    20240424-1 commit

---
+++ src/com/example/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        var name = "yooshin, cha";
+        System.out.println("Hello, " + name);
+    }
+}
+
